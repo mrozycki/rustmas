748c26d91ee7191213f30e017889aa214f06eb83
Mariusz Różycki <mar.rozycki@gmail.com>
2022-10-03 21:37:00 +0200

    Create dummy configurator tool

--- configurator/src/main.rs
+++ configurator/src/main.rs
@@ -1,3 +1,118 @@
-fn main() {
-    rustmas_light_client::print_merry_rustmas();
+use std::{
+    error::Error,
+    fmt::Write,
+    io::{self, Read, Write as IoWrite},
+    path::Path,
+    thread,
+    time::Duration,
+};
+
+use clap::Parser;
+use indicatif::{ProgressBar, ProgressFinish, ProgressIterator, ProgressState, ProgressStyle};
+
+use client::LightClient;
+use rustmas_light_client as client;
+
+type Picture = Vec<u8>;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Arguments {
+    #[arg(short, long, default_value = "lights.csv")]
+    output: String,
+    #[arg(short, long, default_value_t = 500)]
+    lights_count: usize,
+}
+
+fn generate_single_light_frame(index: usize, size: usize) -> client::Frame {
+    client::Frame::new_black(size).with_pixel(index, client::Color::white())
+}
+
+fn take_picture() -> Picture {
+    thread::sleep(Duration::from_millis(1));
+    Vec::new()
+}
+
+fn find_light(_: Picture) -> (usize, usize) {
+    thread::sleep(Duration::from_millis(1));
+    (21, 37)
+}
+
+fn capture_perspective(light_client: &dyn LightClient, lights_count: usize) -> Vec<(usize, usize)> {
+    let mut coords = Vec::new();
+
+    let pb = ProgressBar::new(lights_count as u64)
+        .with_style(
+            ProgressStyle::with_template(
+                "{msg} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {pos}/{len} ({eta})",
+            )
+            .unwrap()
+            .with_key("eta", |state: &ProgressState, w: &mut dyn Write| {
+                write!(w, "{:.1}s", state.eta().as_secs_f64()).unwrap()
+            })
+            .progress_chars("#>-"),
+        )
+        .with_message("Locating lights")
+        .with_finish(ProgressFinish::AndLeave);
+
+    for i in (0..lights_count).progress_with(pb) {
+        let frame = generate_single_light_frame(i, lights_count);
+        if let Err(e) = light_client.display_frame(&frame) {
+            eprintln!("Warning: failed to light up light #{}, skipping", i);
+            continue;
+        }
+
+        let picture = take_picture();
+        coords.push(find_light(picture));
+    }
+
+    coords
+}
+
+fn merge_perspectives(
+    front: Vec<(usize, usize)>,
+    side: Vec<(usize, usize)>,
+) -> Vec<(f64, f64, f64)> {
+    vec![(0.0, 1.0, 0.0); front.len()]
+}
+
+fn save_positions<P: AsRef<Path>>(
+    path: P,
+    positions: &Vec<(f64, f64, f64)>,
+) -> Result<(), Box<dyn Error>> {
+    let mut writer = csv::Writer::from_path(path)?;
+    positions
+        .iter()
+        .map(|light| writer.serialize(light))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Arguments::parse();
+
+    let all_white = client::Frame::new(args.lights_count, client::Color::white());
+
+    let mut stdin = io::stdin();
+    let light_client = client::MockLightClient::new();
+
+    light_client.display_frame(&all_white)?;
+    println!("Position camera to capture lights from the front");
+    print!("Press return to continue...");
+    io::stdout().flush()?;
+    stdin.read(&mut [0u8])?;
+    let front = capture_perspective(&light_client, args.lights_count);
+
+    light_client.display_frame(&all_white)?;
+    println!("Position camera to capture lights from the right-hand side");
+    print!("Press return to continue...");
+    io::stdout().flush()?;
+    stdin.read(&mut [0u8])?;
+    let side = capture_perspective(&light_client, args.lights_count);
+
+    let light_positions = merge_perspectives(front, side);
+    save_positions(args.output, &light_positions)?;
+
+    Ok(())
 }
--- light-client/src/lib.rs
+++ light-client/src/lib.rs
@@ -1,3 +1,98 @@
-pub fn print_merry_rustmas() {
-    println!("Merry Rustmas!");
+use core::fmt;
+use std::cell::{Ref, RefCell};
+
+#[derive(Clone, Copy)]
+pub struct Color {
+    r: u8,
+    g: u8,
+    b: u8,
+}
+
+impl Color {
+    pub fn rgb(r: u8, g: u8, b: u8) -> Self {
+        Self { r, g, b }
+    }
+
+    pub fn gray(shade: u8) -> Self {
+        Self::rgb(shade, shade, shade)
+    }
+
+    pub fn black() -> Self {
+        Self::gray(0)
+    }
+
+    pub fn white() -> Self {
+        Self::gray(255)
+    }
+}
+
+#[derive(Clone)]
+pub struct Frame {
+    pixels: Vec<Color>,
+}
+
+impl Frame {
+    pub fn new(light_count: usize, color: Color) -> Self {
+        Self {
+            pixels: vec![color; light_count],
+        }
+    }
+    pub fn new_black(light_count: usize) -> Self {
+        Self::new(light_count, Color::black())
+    }
+
+    pub fn set_pixel(&mut self, index: usize, color: Color) {
+        self.pixels[index] = color;
+    }
+
+    pub fn with_pixel(mut self, index: usize, color: Color) -> Self {
+        self.pixels[index] = color;
+        self
+    }
+}
+
+impl From<Vec<Color>> for Frame {
+    fn from(pixels: Vec<Color>) -> Self {
+        Self { pixels }
+    }
+}
+
+#[derive(Debug)]
+pub enum LightClientError {
+    Unlikely,
+}
+
+impl fmt::Display for LightClientError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl std::error::Error for LightClientError {}
+
+pub trait LightClient {
+    fn display_frame(&self, frame: &Frame) -> Result<(), LightClientError>;
+}
+
+pub struct MockLightClient {
+    frames: RefCell<Vec<Frame>>,
+}
+
+impl MockLightClient {
+    pub fn new() -> Self {
+        Self {
+            frames: RefCell::new(Vec::new()),
+        }
+    }
+
+    pub fn get_frames(&self) -> Ref<Vec<Frame>> {
+        self.frames.borrow()
+    }
+}
+
+impl LightClient for MockLightClient {
+    fn display_frame(&self, frame: &Frame) -> Result<(), LightClientError> {
+        self.frames.borrow_mut().push(frame.clone());
+        Ok(())
+    }
 }
