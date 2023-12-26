560effe9e64229c4bac6568d8f9815412c43632c
Mariusz Różycki <mar.rozycki@gmail.com>
2023-12-26 13:59:42 +0100

    Refactor animator package

--- animator/src/factory.rs
+++ animator/src/factory.rs
@@ -1,14 +1,15 @@
 use std::{
     collections::HashMap,
     path::{Path, PathBuf},
-    process::{Command, Stdio},
 };
 
-use log::{info, warn};
-use serde::Deserialize;
+use log::warn;
 use thiserror::Error;
 
-use crate::jsonrpc_animation::{AnimationPlugin, AnimationPluginError, JsonRpcEndpoint};
+use crate::{
+    jsonrpc::JsonRpcPlugin,
+    plugin::{AnimationPluginError, PluginConfig, PluginConfigError},
+};
 
 #[derive(Debug, Error)]
 pub enum AnimationFactoryError {
@@ -18,8 +19,8 @@ pub enum AnimationFactoryError {
     #[error("animation not found")]
     AnimationNotFound,
 
-    #[error("malformed animation manifest: {reason}")]
-    InvalidManifest { reason: String },
+    #[error("problem with plugin configuration: {0}")]
+    InvalidPlugin(#[from] PluginConfigError),
 
     #[error("animation failed to start: {0}")]
     AnimationFailedToStart(#[from] std::io::Error),
@@ -28,43 +29,9 @@ pub enum AnimationFactoryError {
     AnimationError(#[from] AnimationPluginError),
 }
 
-#[derive(Clone, Deserialize)]
-pub struct PluginManifest {
-    pub display_name: String,
-}
-
-pub struct Plugin {
-    pub manifest: PluginManifest,
-    pub path: PathBuf,
-}
-
-impl Plugin {
-    fn executable_path(&self) -> PathBuf {
-        #[cfg(windows)]
-        let executable_name = "plugin.exe";
-
-        #[cfg(not(windows))]
-        let executable_name = "plugin";
-
-        self.path.join(executable_name)
-    }
-
-    fn start(&self) -> std::io::Result<JsonRpcEndpoint> {
-        JsonRpcEndpoint::new(self.executable_path())
-    }
-
-    fn is_executable(&self) -> bool {
-        Command::new(self.executable_path())
-            .stdout(Stdio::null())
-            .stdin(Stdio::null())
-            .spawn()
-            .is_ok()
-    }
-}
-
 pub struct AnimationFactory {
     plugin_dir: PathBuf,
-    plugins: HashMap<String, Plugin>,
+    available_plugins: HashMap<String, PluginConfig>,
     points: Vec<(f64, f64, f64)>,
 }
 
@@ -72,7 +39,7 @@ impl AnimationFactory {
     pub fn new<P: AsRef<Path>>(plugin_dir: P, points: Vec<(f64, f64, f64)>) -> Self {
         Self {
             plugin_dir: plugin_dir.as_ref().to_owned(),
-            plugins: HashMap::new(),
+            available_plugins: HashMap::new(),
             points,
         }
     }
@@ -94,27 +61,10 @@ impl AnimationFactory {
                 reason: format!("Glob error: {}", e),
             })
             .and_then(|path| -> Result<_, AnimationFactoryError> {
-                let manifest: PluginManifest =
-                    serde_json::from_slice(&std::fs::read(&path).map_err(|e| {
-                        AnimationFactoryError::InvalidManifest {
-                            reason: format!("IO error: {}", e),
-                        }
-                    })?)
-                    .map_err(|e| {
-                        AnimationFactoryError::InvalidManifest {
-                            reason: e.to_string(),
-                        }
-                    })?;
                 let path = path.parent().unwrap().to_owned();
-                let id = path
-                    .file_name()
-                    .unwrap()
-                    .to_str()
-                    .ok_or(AnimationFactoryError::InternalError {
-                        reason: "Plugins directory path is not valid UTF-8".into(),
-                    })?
-                    .to_owned();
-                Ok((id, Plugin { path, manifest }))
+                let plugin = PluginConfig::new(path)?;
+                let id = plugin.animation_id().to_owned();
+                Ok((id, plugin))
             })
         })
         .collect::<Result<Vec<_>, _>>()?
@@ -125,26 +75,21 @@ impl AnimationFactory {
             warn!("Discovered {} plugins which are not executable. Please make sure the animations were built and have correct permissions.", invalid_plugins.len());
         }
 
-        self.plugins = valid_plugins;
+        self.available_plugins = valid_plugins;
 
         Ok(())
     }
 
-    pub fn list(&self) -> &HashMap<String, Plugin> {
-        &self.plugins
+    pub fn list(&self) -> &HashMap<String, PluginConfig> {
+        &self.available_plugins
     }
 
-    pub fn make(&self, name: &str) -> Result<AnimationPlugin, AnimationFactoryError> {
-        let Some(plugin) = self.plugins.get(name) else {
+    pub fn make(&self, name: &str) -> Result<JsonRpcPlugin, AnimationFactoryError> {
+        let Some(plugin) = self.available_plugins.get(name) else {
             return Err(AnimationFactoryError::AnimationNotFound);
         };
 
-        info!(
-            "Trying to start plugin app '{}' from directory '{:?}'",
-            name, plugin.path
-        );
-
-        Ok(AnimationPlugin::new(plugin.start()?, self.points.clone())?)
+        Ok(JsonRpcPlugin::new(plugin, self.points.clone())?)
     }
 
     pub fn points(&self) -> &[(f64, f64, f64)] {
