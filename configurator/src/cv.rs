54c1d96193f3d95dc1f624b56e56770d6256b625
Krzysztof Mazur <krzmazur1@gmail.com>
2024-10-23 22:21:45 +0200

    WIP opencv removal

--- configurator/src/cv.rs
+++ configurator/src/cv.rs
@@ -3,13 +3,15 @@ use std::path::{Path, PathBuf};
 use std::sync::{Arc, Mutex};
 use std::{error::Error, time::Duration};
 
+use image::buffer::ConvertBuffer;
+use image::{DynamicImage, GrayImage, Luma, Rgb, RgbImage};
+use imageproc::drawing::draw_hollow_circle_mut;
+use imageproc::integral_image::ArrayData;
+use imageproc::map::map_colors2;
+use imageproc::morphology::{erode, grayscale_erode, Mask};
+use imageproc::template_matching::find_extremes;
 use log::debug;
-use opencv::imgproc::COLOR_GRAY2RGB;
-use opencv::{
-    core, highgui, imgproc,
-    prelude::{Mat, MatTraitConst},
-    videoio::{self, VideoCapture, VideoCaptureTrait, VideoCaptureTraitConst},
-};
+
 use tokio::task::JoinHandle;
 
 use crate::capture::WithConfidence;
@@ -30,45 +32,39 @@ impl Error for CameraError {}
 
 #[derive(Default)]
 pub struct Picture {
-    inner: Mat,
+    inner: image::RgbImage,
 }
 
 impl Picture {
     pub fn mark(&mut self, coords: &WithConfidence<(usize, usize)>) -> Result<(), Box<dyn Error>> {
         let color = if coords.confident() {
-            core::VecN::new(0.0, 255.0, 0.0, 255.0) // green
+            Rgb([0u8, 255u8, 0u8]) // green
         } else {
-            core::VecN::new(0.0, 0.0, 255.0, 255.0) // red
+            Rgb([255u8, 0u8, 0u8]) // red
         };
-        imgproc::circle(
+        draw_hollow_circle_mut(
             &mut self.inner,
-            core::Point::new(coords.inner.0 as i32, coords.inner.1 as i32),
+            (coords.inner.0 as i32, coords.inner.1 as i32),
             20,
             color,
-            2,
-            imgproc::LINE_AA,
-            0,
-        )?;
+        );
         Ok(())
     }
 
     pub fn save_to_file<P: AsRef<Path>>(&self, path: P) -> Result<(), Box<dyn Error>> {
         let path = path.as_ref().to_str().ok_or("Bad path provided")?;
         debug!("Writing file: {}", path);
-        opencv::imgcodecs::imwrite(path, &self.inner, &opencv::core::Vector::default())?;
+        self.inner.save(path)?;
         Ok(())
     }
 
     pub fn from_file(path: &Path) -> Result<Self, Box<dyn Error>> {
-        Ok(Self::from(opencv::imgcodecs::imread(
-            path.to_str().ok_or("Image path isn't valid unicode")?,
-            1,
-        )?))
+        Ok(image::ImageReader::open(path)?.decode()?.into_rgb8().into())
     }
 }
 
-impl From<Mat> for Picture {
-    fn from(inner: Mat) -> Self {
+impl From<image::RgbImage> for Picture {
+    fn from(inner: image::RgbImage) -> Self {
         Self {
             inner: inner.clone(),
         }
@@ -76,63 +72,67 @@ impl From<Mat> for Picture {
 }
 
 pub struct Camera {
-    camera_handle: Arc<Mutex<videoio::VideoCapture>>,
-    _grabber_thread_join: JoinHandle<()>,
+    // camera_handle: Arc<Mutex<videoio::VideoCapture>>,
+    // _grabber_thread_join: JoinHandle<()>,
 }
 
 impl Camera {
-    pub fn new_from_video_capture(mut capture: VideoCapture) -> Result<Self, Box<dyn Error>> {
-        capture.set(videoio::CAP_PROP_BUFFERSIZE, 1.0)?;
+    // pub fn new_from_video_capture(mut capture: VideoCapture) -> Result<Self, Box<dyn Error>> {
+    //     capture.set(videoio::CAP_PROP_BUFFERSIZE, 1.0)?;
 
-        if !videoio::VideoCapture::is_opened(&capture)? {
-            return Err(Box::new(CameraError::InitializeError));
-        }
-        let camera_handle = Arc::new(Mutex::new(capture));
-        let camera_handle_clone = camera_handle.clone();
-        let _grabber_thread_join = tokio::spawn(async move {
-            loop {
-                let _ = camera_handle_clone.lock().unwrap().grab();
-                tokio::time::sleep(Duration::from_millis(10)).await;
-            }
-        });
-        Ok(Self {
-            camera_handle,
-            _grabber_thread_join,
-        })
-    }
+    //     if !videoio::VideoCapture::is_opened(&capture)? {
+    //         return Err(Box::new(CameraError::InitializeError));
+    //     }
+    //     let camera_handle = Arc::new(Mutex::new(capture));
+    //     let camera_handle_clone = camera_handle.clone();
+    //     let _grabber_thread_join = tokio::spawn(async move {
+    //         loop {
+    //             let _ = camera_handle_clone.lock().unwrap().grab();
+    //             tokio::time::sleep(Duration::from_millis(10)).await;
+    //         }
+    //     });
+    //     Ok(Self {
+    //         camera_handle,
+    //         _grabber_thread_join,
+    //     })
+    // }
 
     pub fn new_default() -> Result<Self, Box<dyn Error>> {
-        Self::new_local(0)
+        // Self::new_local(0)
+        Ok(Self {})
     }
 
     pub fn new_local(index: i32) -> Result<Self, Box<dyn Error>> {
-        let cam = videoio::VideoCapture::new(index, videoio::CAP_ANY)?; // 0 is the default camera
-        Self::new_from_video_capture(cam)
+        // let cam = videoio::VideoCapture::new(index, videoio::CAP_ANY)?; // 0 is the default camera
+        // Self::new_from_video_capture(cam)
+        Ok(Self {})
     }
 
     pub fn new_from_file(path: &str) -> Result<Self, Box<dyn Error>> {
-        let cam = videoio::VideoCapture::from_file(path, videoio::CAP_ANY)?; // 0 is the default camera
-        Self::new_from_video_capture(cam)
+        // let cam = videoio::VideoCapture::from_file(path, videoio::CAP_ANY)?; // 0 is the default camera
+        // Self::new_from_video_capture(cam)
+        Ok(Self {})
     }
 
     pub fn capture(&mut self) -> Result<Picture, Box<dyn Error>> {
-        let mut frame = Mat::default();
-        let mut camera_handle = self.camera_handle.lock().unwrap();
-        camera_handle.read(&mut frame)?;
+        // let mut frame = Mat::default();
+        // let mut camera_handle = self.camera_handle.lock().unwrap();
+        // camera_handle.read(&mut frame)?;
 
-        if frame.size()?.width > 0 {
-            Ok(frame.into())
-        } else {
-            Err(Box::new(CameraError::CaptureError))
-        }
+        // if frame.size()?.width > 0 {
+        //     Ok(frame.into())
+        // } else {
+        //     Err(Box::new(CameraError::CaptureError))
+        // }
+        todo!();
     }
 }
 
-impl Drop for Camera {
-    fn drop(&mut self) {
-        self.camera_handle.lock().unwrap().release().unwrap();
-    }
-}
+// impl Drop for Camera {
+//     fn drop(&mut self) {
+//         self.camera_handle.lock().unwrap().release().unwrap();
+//     }
+// }
 
 pub struct Display {
     window_name: String,
@@ -140,8 +140,8 @@ pub struct Display {
 
 impl Display {
     pub fn new(window_name: &str) -> Result<Self, Box<dyn Error>> {
-        highgui::named_window(window_name, highgui::WINDOW_AUTOSIZE)?;
-        highgui::set_window_property(window_name, highgui::WND_PROP_TOPMOST, 1.0)?;
+        // highgui::named_window(window_name, highgui::WINDOW_AUTOSIZE)?;
+        // highgui::set_window_property(window_name, highgui::WND_PROP_TOPMOST, 1.0)?;
 
         Ok(Self {
             window_name: window_name.to_owned(),
@@ -149,20 +149,21 @@ impl Display {
     }
 
     pub fn show(&self, picture: &Picture) -> Result<(), Box<dyn Error>> {
-        highgui::imshow(&self.window_name, &picture.inner)?;
+        // highgui::imshow(&self.window_name, &picture.inner)?;
         Ok(())
     }
 
     pub fn wait_for(&self, duration: Duration) -> Result<i32, Box<dyn Error>> {
-        Ok(highgui::wait_key(duration.as_millis() as i32)?)
+        // Ok(highgui::wait_key(duration.as_millis() as i32)?)
+        Ok(0)
     }
 }
 
 impl Drop for Display {
     fn drop(&mut self) {
-        highgui::destroy_window(&self.window_name).unwrap();
+        // highgui::destroy_window(&self.window_name).unwrap();
         // hack to get the window to close, see https://stackoverflow.com/a/50710994
-        highgui::wait_key(1).unwrap();
+        // highgui::wait_key(1).unwrap();
     }
 }
 
@@ -178,78 +179,44 @@ pub fn find_light_from_diff_with_output(
     led_picture: &Picture,
     output_dir: Option<PathBuf>,
 ) -> Result<WithConfidence<(usize, usize)>, Box<dyn Error>> {
-    let mut base_gray = Mat::default();
-    imgproc::cvt_color(
-        &base_picture.inner,
-        &mut base_gray,
-        imgproc::COLOR_BGR2GRAY,
-        0,
-    )?;
+    let base_gray: GrayImage = base_picture.inner.convert();
     if let Some(output_dir) = &output_dir {
-        Picture::from(base_gray.clone())
+        Picture::from(base_gray.clone().convert())
             .save_to_file(output_dir.with_file_name("1_base_grayscale.jpg"))?;
     }
-    let mut led_gray = Mat::default();
-    imgproc::cvt_color(
-        &led_picture.inner,
-        &mut led_gray,
-        imgproc::COLOR_BGR2GRAY,
-        0,
-    )?;
+
+    let led_gray: GrayImage = led_picture.inner.convert();
     if let Some(output_dir) = &output_dir {
-        Picture::from(led_gray.clone())
+        Picture::from(led_gray.clone().convert())
             .save_to_file(output_dir.with_file_name("2_led_grayscale.jpg"))?;
     }
-    let mut diff = Mat::default();
-    core::absdiff(&base_gray, &led_gray, &mut diff)?;
+
+    let diff: GrayImage = map_colors2(&base_gray, &led_gray, |a, b| Luma([a[0].abs_diff(b[0])]));
+
     if let Some(output_dir) = &output_dir {
-        Picture::from(diff.clone()).save_to_file(output_dir.with_file_name("3_diff.jpg"))?;
+        Picture::from(diff.clone().convert())
+            .save_to_file(output_dir.with_file_name("3_diff.jpg"))?;
     }
 
     // erode to remove the noise
-    let mut eroded = Mat::default();
-    let kernel = Mat::default();
-    let anchor = core::Point::new(-1, -1); // default in C++ implementation
-    let border_value = imgproc::morphology_default_border_value()?;
-    imgproc::erode(
-        &diff,
-        &mut eroded,
-        &kernel,
-        anchor,
-        1,
-        core::BORDER_CONSTANT,
-        border_value,
-    )?;
+    let eroded = grayscale_erode(&diff, &Mask::diamond(3u8));
+
     if let Some(output_dir) = &output_dir {
-        Picture::from(eroded.clone()).save_to_file(output_dir.with_file_name("4_eroded.jpg"))?;
+        Picture::from(eroded.clone().convert())
+            .save_to_file(output_dir.with_file_name("4_eroded.jpg"))?;
     }
 
-    let mut max_loc = core::Point::default();
-    let mut max_val: f64 = 0.0;
-    opencv::core::min_max_loc(
-        &eroded,
-        None,
-        Some(&mut max_val),
-        None,
-        Some(&mut max_loc),
-        &Mat::default(),
-    )?;
-    if max_loc.x < 0 || max_loc.y < 0 {
-        // OpenCV might return (-1,-1) if it can't find anything
-        return Ok(WithConfidence::<(usize, usize)> {
-            inner: (0, 0),
-            confidence: f64::NEG_INFINITY,
-        });
-    }
+    let extremes = find_extremes(&eroded);
+
+    let max_loc = extremes.max_value_location;
+    let max_val = extremes.max_value;
+
     let result = WithConfidence::<(usize, usize)> {
-        inner: (max_loc.x as usize, max_loc.y as usize),
-        confidence: max_val / 255.0,
+        inner: (max_loc.0 as usize, max_loc.1 as usize),
+        confidence: max_val as f64 / 255.0,
     };
     if let Some(output_dir) = &output_dir {
-        // eroded image is in grayscale but we want to mark it in color, so we convert it
-        let mut eroded_color = Mat::default();
-        imgproc::cvt_color(&eroded, &mut eroded_color, COLOR_GRAY2RGB, 0)?;
-        let mut pic = Picture::from(eroded_color);
+        let mut pic = Picture::from(eroded.convert());
         pic.mark(&result)?;
         pic.save_to_file(output_dir.with_file_name("5_eroded_marked.jpg"))?;
     }
