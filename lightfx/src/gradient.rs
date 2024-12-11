b08e5efa43f5b9031d2a5a29812eac20e7662708
pnwkw <90510234+pnwkw@users.noreply.github.com>
2024-12-11 22:10:27 +0100

    Optimize Particle Fire animation

--- lightfx/src/gradient.rs
+++ lightfx/src/gradient.rs
@@ -1,20 +1,34 @@
 use crate::Color;
 
 pub struct Gradient {
-    colors: Vec<Color>,
+    lut: Vec<Color>,
 }
 
 impl Gradient {
-    pub fn new(colors: Vec<Color>) -> Self {
-        Self { colors }
+    pub fn new(colors: &[Color]) -> Self {
+        let lut = (0..=255)
+            .map(|d| Self::compute(colors, d as f64 / 255.0))
+            .collect();
+        Self { lut }
     }
 
-    pub fn at(&self, d: f64) -> Color {
-        let d = d.clamp(0.0, 1.0) * (self.colors.len() as f64 - 1.0);
+    fn compute(colors: &[Color], d: f64) -> Color {
+        let d = d.clamp(0.0, 1.0) * (colors.len() as f64 - 1.0);
 
         let left = d.floor() as usize;
         let right = d.ceil() as usize;
         let d = d.fract();
-        self.colors[left].lerp(&self.colors[right], d)
+        colors[left].lerp(&colors[right], d)
+    }
+
+    pub fn at(&self, d: f64) -> Color {
+        let d = (d.clamp(0.0, 1.0) * 255.0) as usize;
+        self.lut[d]
+    }
+}
+
+impl<const N: usize> From<[Color; N]> for Gradient {
+    fn from(colors: [Color; N]) -> Self {
+        Self::new(&colors)
     }
 }
